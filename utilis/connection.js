a77f093071c370ad583ef53581e220bed9d6b3e4
Luis Felipe Cruz Esteban <125835988+berserkTEBAN@users.noreply.github.com>
2024-04-04 19:53:34 -0600

    Agregados archivos de la carpeta principal

---
+++ utilis/connection.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+//const plotly = require('plotly.js');
+//se puede mandar esa uri en una env key para mas seguridad pero ya despuesito xd
+
+const connectMongoDB = () => {
+    //esta se saca de mongo db al crear una cuenta y un cluster,nose por que la nombre test lmao u-u
+    const linkMongoDB = "mongodb+srv://luisfelipecruzesteban398:luis@cluster0.pc1l8ww.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    
+    mongoose.set('strictQuery', false);
+    mongoose.connect(linkMongoDB)
+    .then(() => {
+        console.log('Conectado a mongoDB');
+    })
+    .catch((e)=>{
+        console.log('Error en la conexion a mongoDb'+e);
+    });
+}
+
+module.exports = connectMongoDB;
